81c01afd3392eb162e6fd56ad2c0db24280d0964
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-11 21:07:47 -0700

    setup DB_URL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "localhost:3000",
-  API_BASE_URL:
-    process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://wes@localhost/bjj-symposium",
 };
