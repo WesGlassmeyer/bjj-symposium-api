50db7c096e76dc1ba49f5c0e353251b4d610dbe6
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-20 20:35:02 -0700

    Update fav_items-service.js

--- src/fav_items/fav_items-service.js
+++ src/fav_items/fav_items-service.js
@@ -34,90 +34,17 @@ const FavItemsService = {
           .returning("*");
       })
       .then((newRatingRows) => {
-        console.log(newRatingRows, "aaaaaaaaaaaaaaaaaaa");
         return knex.select("id").from("tags").whereIn("name", newTags);
-        // knex.insert(newTags).into("fav_items_tags_pivot")
       })
       .then((parsedTags) => {
-        console.log(parsedTags, "bbbbbbbbbbbbbbb");
         for (i = 0; i < parsedTags.length; i++) {
-          return knex
+          knex
             .insert({ tag_id: parsedTags[i].id, fav_items_id: favItem.id })
-            .into("fav_items_tags_pivot");
+            .into("fav_items_tags_pivot")
+            .then();
         }
+        return favItem;
       });
-  },
-
-  // return (
-  //   knex
-  //     .select("*")
-  //     .from("fav_items_tags_pivot")
-  //     // .whereIn("tag_id", parsedTags.id)
-  //     .whereNot("fav_items_id", favItem.id)
-  //     .then((matchedTags) => {
-  //       console.log(matchedTags, "ccccccccccccccccccc");
-  //       return matchedTags;
-  //     })
-  // );
-
-  //{
-  //   return knex
-  //     .insert(newItem)
-  //     .into("fav_items")
-  //     .returning("*")
-  //     .then((rows) => {
-  //       console.log(newTags);
-  //       knex
-  //         .insert({ fav_items_id: rows[0].id, value: newRating.rating })
-  //         .into("ratings")
-  //         .returning("*")
-  //         .then((row) => {
-  //           return row;
-  //         });
-
-  //       return rows[0];
-  //     });
-  // },
-
-  //
-  //       .then((favVideoList) => {
-  //         consol knex
-  //       .select("youtube_id")
-  //       .from("fav_items")
-  //       .where("youtube_id", newItem.youtube_id)e.log(favVideoList);
-  //         if (favVideoList.length === 0) {
-  //           return knex
-  //             .insert(newItem)
-  //             .into("fav_items")
-  //             .returning("*")
-  //             .then((rows) => {
-  //               console.log(newTags);
-  //               knex
-  //                 .insert(
-  //                   { value: newRating.rating },
-  //                   { fav_items_id: rows[0].id }
-  //                 )
-  //                 .into("ratings")
-  //                 .returning("*")
-  //                 .then((row) => {
-  //                   return row;
-  //                 });
-  //               console.log("inserted video --------------------------");
-  //               return rows[0];
-  //             });
-  //         }
-  //         console.log("_____________________video not inserted");
-  //       });
-  //   },
-
-  getById(knex, id) {
-    return knex.from("fav_items").select("*").where("id", id).first();
-  },
-  deleteItem(knex, id) {
-    return knex("fav_items").where({ id }).delete();
-  },
-  updateItem(knex, id, newItemFields) {
-    return knex("fav_items").where({ id }).update(newItemFields);
   },
 };
 
