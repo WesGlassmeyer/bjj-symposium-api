6599ad1833a407b5c097ed16191a8269dc57ae3e
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-21 12:23:03 -0700

    Update fav_items-fixtures.js

--- test/fav_items-fixtures.js
+++ test/fav_items-fixtures.js
@@ -1,29 +1,169 @@
 function makeFavItemsArray() {
   return [
     {
-      id: 1,
       title:
         "The 3 Most Important Jiu Jitsu Techniques For A BJJ White Belt by John Danaher",
       youtube_id: "QBqwipFt0Ww",
       thumbnail: "https://i.ytimg.com/vi/QBqwipFt0Ww/default.jpg",
+    },
+    {
+      title: "5 Tips To Pass ANY Guard by John Danaher",
+      youtube_id: "ODuQCA88oY4",
+      thumbnail: "https://i.ytimg.com/vi/ODuQCA88oY4/default.jpg",
+    },
+    {
+      title: "BJJ Moves: Arm Bar From Guard by John Danaher",
+      youtube_id: "pQ43Oy5k9yQ",
+      thumbnail: "https://i.ytimg.com/vi/pQ43Oy5k9yQ/default.jpg",
+    },
+  ];
+}
+
+function makeExpectedFavItemsArray() {
+  return [
+    {
+      id: 1,
+      title:
+        "The 3 Most Important Jiu Jitsu Techniques For A BJJ White Belt by John Danaher",
       rating: 1,
-      tags: ["Mount", "Back Mount", "Side Control"],
+      youtube_id: "QBqwipFt0Ww",
+      tags: ["Mount", "Attack", "Side Control"],
+      thumbnail: "https://i.ytimg.com/vi/QBqwipFt0Ww/default.jpg",
     },
     {
       id: 2,
       title: "5 Tips To Pass ANY Guard by John Danaher",
-      youtube_id: "ODuQCA88oY4",
-      thumbnail: "https://i.ytimg.com/vi/ODuQCA88oY4/default.jpg",
       rating: 2,
-      tags: ["Guard", "Choke", "Arm Bar"],
+      youtube_id: "ODuQCA88oY4",
+      tags: ["Back Mount", "Guard", "Reversal"],
+      thumbnail: "https://i.ytimg.com/vi/ODuQCA88oY4/default.jpg",
     },
     {
       id: 3,
       title: "BJJ Moves: Arm Bar From Guard by John Danaher",
-      youtube_id: "pQ43Oy5k9yQ",
-      thumbnail: "https://i.ytimg.com/vi/pQ43Oy5k9yQ/default.jpg",
       rating: 3,
-      tags: ["Kimura", "Triangle", "Leg Locks"],
+      youtube_id: "pQ43Oy5k9yQ",
+      tags: ["Choke", "Kimura", "Leg Locks"],
+      thumbnail: "https://i.ytimg.com/vi/pQ43Oy5k9yQ/default.jpg",
+    },
+  ];
+}
+
+function makeRatingsArray() {
+  return [
+    {
+      value: "1",
+      fav_items_id: 1,
+    },
+    {
+      value: "2",
+      fav_items_id: 2,
+    },
+    {
+      value: "3",
+      fav_items_id: 3,
+    },
+    {
+      value: "4",
+      fav_items_id: 3,
+    },
+  ];
+}
+
+function makeTagsArray() {
+  return [
+    {
+      name: "Mount",
+      category: "Position",
+    },
+    {
+      name: "Back Mount",
+      category: "Position",
+    },
+    {
+      name: "Side Control",
+      category: "Position",
+    },
+    {
+      name: "Guard",
+      category: "Position",
+    },
+    {
+      name: "Choke",
+      category: "Submissions",
+    },
+    {
+      name: "Arm bar",
+      category: "Submissions",
+    },
+    {
+      name: "Kimura",
+      category: "Submissions",
+    },
+    {
+      name: "Triangle",
+      category: "Submissions",
+    },
+    {
+      name: "Leg Locks",
+      category: "Submissions",
+    },
+    {
+      name: "Escape",
+      category: "Actions",
+    },
+    {
+      name: "Control",
+      category: "Actions",
+    },
+    {
+      name: "Reversal",
+      category: "Actions",
+    },
+    {
+      name: "Attack",
+      category: "Actions",
+    },
+  ];
+}
+
+function makeFavItemsTagsPivot() {
+  return [
+    {
+      fav_items_id: 1,
+      tag_id: 1,
+    },
+    {
+      fav_items_id: 1,
+      tag_id: 13,
+    },
+    {
+      fav_items_id: 1,
+      tag_id: 3,
+    },
+    {
+      fav_items_id: 2,
+      tag_id: 2,
+    },
+    {
+      fav_items_id: 2,
+      tag_id: 4,
+    },
+    {
+      fav_items_id: 2,
+      tag_id: 11,
+    },
+    {
+      fav_items_id: 3,
+      tag_id: 5,
+    },
+    {
+      fav_items_id: 3,
+      tag_id: 7,
+    },
+    {
+      fav_items_id: 3,
+      tag_id: 9,
     },
   ];
 }
@@ -54,5 +194,9 @@ function makeMaliciousItem() {
 
 module.exports = {
   makeFavItemsArray,
+  makeExpectedFavItemsArray,
+  makeRatingsArray,
+  makeTagsArray,
+  makeFavItemsTagsPivot,
   makeMaliciousItem,
 };
