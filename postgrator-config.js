834635286ab23f85ffc20c0da505d8d8c942b837
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-24 17:33:05 -0700

    Update postgrator-config.js

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 module.exports = {
   migrationsDirectory: "migrations",
   driver: "pg",
-  connectionString: process.env.DB_URL,
+  ssl: true,
+  connectionString: process.env.DATABASE_URL,
 };
